1c819d29941c0ae4be45f6ee46c298677781ceb8
daniel starns <danielstarns@hotmail.com>
2022-10-12 18:44:21 +0200

    test: *

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,4 +4,8 @@ describe("graphql-otel", () => {
   test("should export traceDirective", () => {
     expect(pkg.traceDirective).toBeDefined();
   });
+
+  test("should export GraphQLOTELContext", () => {
+    expect(pkg.GraphQLOTELContext).toBeDefined();
+  });
 });
