7697039264bc061a62acbe57591e300beafd354f
danstarns <danielstarns@hotmail.com>
2023-04-18 16:25:58 +0100

    feat: add working contact form

--- graphql-otel.com/server.js
+++ graphql-otel.com/server.js
@@ -4,15 +4,39 @@ dotenv.config({ path: "./.env" });
 import express from "express";
 import cors from "cors";
 import expressStaticGzip from "express-static-gzip";
+import sgMail from "@sendgrid/mail";
 
 const config = {
   STATIC_FOLDER: process.env.STATIC_FOLDER,
   HTTP_PORT: process.env.HTTP_PORT,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  SENDGRID_FROM: process.env.SENDGRID_FROM,
 };
+
+sgMail.setApiKey(config.SENDGRID_API_KEY);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.post("/contact", async (req, res) => {
+  const { email, name, message } = req.body;
+
+  const msg = {
+    to: [email, config.SENDGRID_FROM],
+    from: config.SENDGRID_FROM,
+    subject: "GraphQL Tracing with OpenTelemetry",
+    text: `Hey ${name} ${email}, I'm Dan. Thanks for contacting about support with GraphQL OTEL. We will get back to you shortly. ${message}`,
+  };
+
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.end();
+});
+
 app.use(expressStaticGzip(config.STATIC_FOLDER, {}));
 app.get("*", expressStaticGzip(config.STATIC_FOLDER, {}));
 app.use("*", expressStaticGzip(config.STATIC_FOLDER, {}));
@@ -23,7 +47,7 @@ function main() {
       console.error(err);
     }
 
-    console.log(`istening at http://localhost:${config.HTTP_PORT}`);
+    console.log(`Listening at http://localhost:${config.HTTP_PORT}`);
   });
 }
 
--- graphql-otel.com/src/components/Contact.tsx
+++ graphql-otel.com/src/components/Contact.tsx
@@ -1,6 +1,62 @@
+import { useCallback, useState } from "react";
+import { API_URL } from "../config";
+
+export async function signup(body: {
+  name: string;
+  email: string;
+  message: string;
+}) {
+  const response = await fetch(`${API_URL}/contact`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (response.status !== 200 || !response.ok) {
+    throw new Error(await response.text());
+  }
+}
+
 export function Contact() {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [sent, setSent] = useState(false);
+
+  const send = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setLoading(true);
+
+      try {
+        const body = {
+          // @ts-ignore
+          email: e.target.elements.email.value as string,
+          // @ts-ignore
+          name: e.target.elements.name.value as string,
+          // @ts-ignore
+          message: e.target.elements.message.value as string,
+        };
+
+        await signup(body);
+        setSent(true);
+      } catch (error) {
+        const e = error as Error;
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [setError, setLoading]
+  );
+
+  if (error) {
+    return <></>;
+  }
+
   return (
-    <form className="p-0 m-0" onSubmit={() => {}}>
+    <form className="p-0 m-0" onSubmit={send}>
       <div className="flex flex-col gap-10 text-graphql-otel-dark">
         <input
           className="bg-white p-3 w-96 h-12 placeholder-graphql-otel-dark"
@@ -8,7 +64,7 @@ export function Contact() {
           name="name"
           id="name"
           placeholder="Name"
-          disabled={true}
+          disabled={Boolean(loading || sent || error)}
           required={true}
         />
 
@@ -18,7 +74,7 @@ export function Contact() {
           name="email"
           placeholder="Email"
           id="email"
-          disabled={true}
+          disabled={Boolean(loading || sent || error)}
           required={true}
         />
         <textarea
@@ -26,13 +82,13 @@ export function Contact() {
           name="message"
           placeholder="I would like help tracing my GraphQL API."
           id="message"
-          disabled={true}
+          disabled={Boolean(loading || sent || error)}
           required={true}
         />
 
         <button
           type="submit"
-          disabled={true}
+          disabled={Boolean(loading || sent || error)}
           className="ml-auto italic bg-white p-3 submit w-3/6 py-2 underline decoration-graphql-otel-green underline-offset-8"
         >
           Send
