654456abde356b72aafb14512465d510a6b82cb3
danstarns <danielstarns@hotmail.com>
2023-04-18 18:27:55 +0100

    style: mobile

--- graphql-otel.com/src/components/Intro.tsx
+++ graphql-otel.com/src/components/Intro.tsx
@@ -7,7 +7,7 @@ export function Intro() {
       <Container>
         <div className="flex flex-col lg:flex-row justify-between gap-10">
           <div className="text-white d-flex flex-col">
-            <h1 className="text-5xl mb-10 bold">GraphQL OTEL</h1>
+            <h1 className="text-4xl lg:text-5xl mb-10 bold">GraphQL OTEL</h1>
             <p className="mb-10 italic">
               Trace your GraphQL API with OpenTelemetry.
             </p>
@@ -22,7 +22,7 @@ export function Intro() {
             </button>
           </div>
 
-          <div className="w-5/6 mx-auto lg:mx-0 lg:w-3/6 mb-10 mt-10">
+          <div className="w-80 mx-auto lg:mx-0 lg:w-3/6 my-10">
             <img src={examplepatients} alt="code" />
           </div>
         </div>
