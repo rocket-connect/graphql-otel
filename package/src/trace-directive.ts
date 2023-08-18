3dfb6091b11a1018f14ffda53a0800f30db7656b
danstarns <danielstarns@hotmail.com>
2023-08-18 20:36:18 +0100

    feat: support for opentelemetry spec conventions

--- package/src/trace-directive.ts
+++ package/src/trace-directive.ts
@@ -6,6 +6,15 @@ import { Span } from "@opentelemetry/sdk-trace-base";
 import { runInSpan } from "./run-in-span";
 import safeJSON from "safe-json-stringify";
 
+// Matching spec https://opentelemetry.io/docs/specs/otel/trace/semantic_conventions/instrumentation/graphql/
+export enum AttributeName {
+  OPERATION_NAME = "graphql.operation.name",
+  OPERATION_TYPE = "graphql.operation.type",
+  DOCUMENT = "graphql.document",
+  ARGS = "graphql.operation.args", // Non-Spec attribute
+  CONTEXT = "graphql.operation.context", // Non-Spec attribute
+}
+
 export function traceDirective(directiveName = "trace") {
   return {
     typeDefs: `directive @${directiveName} on FIELD_DEFINITION`,
@@ -43,11 +52,18 @@ export function traceDirective(directiveName = "trace") {
 
               const parentSpan = context.parentSpan as Span | undefined;
 
-              const isRoot = ["Query", "Mutation"].includes(
+              const isRoot = ["Query", "Mutation", "Subscription"].includes(
                 info.parentType.name
               );
 
-              const name = `${info.parentType.name}:${fieldConfig.astNode?.name.value}`;
+              let name = "";
+              if (isRoot) {
+                name = `${info.parentType.name.toLowerCase()} ${
+                  info.operation.name?.value || info.fieldName
+                }`;
+              } else {
+                name = `${info.parentType.name} ${fieldConfig.astNode?.name.value}`;
+              }
 
               const attributeContext = {
                 ...context,
@@ -68,21 +84,20 @@ export function traceDirective(directiveName = "trace") {
                   context: traceCTX,
                   tracer: internalCtx.tracer,
                   parentSpan,
-                  ...(isRoot
-                    ? {
-                        attributes: {
-                          query: print(info.operation),
-                          ...(internalCtx.includeVariables
-                            ? { variables: safeJSON(args) }
-                            : {}),
-                          ...(internalCtx.includeContext
-                            ? {
-                                context: safeJSON(attributeContext),
-                              }
-                            : {}),
-                        },
-                      }
-                    : {}),
+                  attributes: {
+                    [AttributeName.OPERATION_NAME]: info.fieldName,
+                    [AttributeName.OPERATION_TYPE]:
+                      info.operation.operation.toLowerCase(),
+                    [AttributeName.DOCUMENT]: print(info.operation),
+                    ...(internalCtx.includeVariables
+                      ? { [AttributeName.ARGS]: safeJSON(args) }
+                      : {}),
+                    ...(internalCtx.includeContext
+                      ? {
+                          [AttributeName.CONTEXT]: safeJSON(attributeContext),
+                        }
+                      : {}),
+                  },
                 },
                 async (span) => {
                   if (!internalCtx.getRootSpan()) {
